ba7425d862ae0a785efcc32664303e5bd5c6e215
Gagan Awhad <regist.gagan@gmail.com>
2010-09-18 13:27:43 -0500

    I have only cleaned the way GNFA class looks and made the commenting slightly better. There is no change I have made in the functionality of the class.

--- GNFA.cpp
+++ GNFA.cpp
@@ -1,8 +1,8 @@
 /*
  *  GNFA.cpp
  *  This GNFA does not the gGNFA as described by the text. 
- *  crudely, it provides a way to store things that are relevant to state machines in general
- *  It then has some validity tests to check if it is indeed a gNFA or NFA or DFA of the text. 
+ *  Crudely put, it provides a way to store things that are relevant to state machines in general
+ *  It does have validity tests to check if it is indeed a gNFA or NFA or DFA of the text. 
  *  Created by Gagan Awhad on 10/31/09.
  *  Copyright 2009 Unversity of Minnesota. All rights reserved.
  *
@@ -13,22 +13,22 @@
 #include "GNFA.h"
 #include <iostream>
 
+//This constructor creates a simple GNFA class that has states and has all possible transition functions as null set
 
-	//This constructor creates a simple GNFA class that has states and has all possible transition functions as null set
 GNFA::GNFA(int n){
-	this->noOfStates = n;
-	this->acceptStates.resize(noOfStates);
-	
-	
-	
-	for (int i =0; i< noOfStates; i++){
-		transVector temp;
-		for (int j = 0; j< noOfStates; j++){
-			//cout<<i<<j<<endl;
-			temp.push_back(new leafNode(NULLSET));
-		}	
-		transFunction.push_back(temp);
-	}
+  this->noOfStates = n;
+  this->acceptStates.resize(noOfStates);
+  
+  for (int i = 0; i< noOfStates; i++){
+    transVector temp;
+    for (int j = 0; j< noOfStates; j++){
+      //cout<<i<<j<<endl; //used for the sake of debugging
+      temp.push_back(new leafNode(NULLSET));
+    }	
+
+    transFunction.push_back(temp);
+  }
+  
   cout<<"this is 0 4 on creation";
   transFunction[0][4]->display();
   cout<<endl;
@@ -36,21 +36,18 @@ GNFA::GNFA(int n){
 }
 	
 
-// Here do you want to create another constructor in which the argument n the number of states and a state of functions is given?
-	
-	
-bool GNFA::deleteTransition(int i, int j){
-	transFunction[i][j]->cascadeDel();
-	return true;
-}
 
-//functions take two states and regular expression and then set the transiontion function
+//Take two states and regular expression and then set the transiontion function
 bool GNFA::setTransition (int i, int j, regexNode* k) {
-	transFunction[i][j] = k;	
+  transFunction[i][j] = k;	
   return true;
 }
-	
 
+// Here do you want to create another constructor in which the argument n the number of states and a state of functions is given?
+bool GNFA::deleteTransition(int i, int j){
+  transFunction[i][j]->cascadeDel();
+  return true;
+}
 
 
 bool GNFA::clearTransition (int i, int j){
@@ -60,23 +57,21 @@ bool GNFA::clearTransition (int i, int j){
 }
 	
 
-
-
-
 //function to set a final state
 bool GNFA::setAcceptState(int i){
-	if(i < this->noOfStates){
-    acceptStates[i] = 1; // i-1 to get the right state
-		return true;
+  if(i < this->noOfStates){
+    acceptStates[i] = 1; 
+    return true;
   }
-	else{
-		cout<<"Incorrect accpet state"<<endl;
-		return false;
-	}
-  
+  else {
+    cout<<"Incorrect accept state. States are from State 0 to State n-1"<<endl;
+    return false;
+  }
 }
 
 
+
+//Function to check if state is an accept state
 bool GNFA::isAcceptState(int i){
   if(acceptStates[i]==1){
     return true;
@@ -89,17 +84,17 @@ bool GNFA::isAcceptState(int i){
 
 
 
-//set a function to set a non final state
+//Function to set a State to a non final state
 bool GNFA::resetAcceptState(int i){
-   if(i < this->noOfStates){
-    acceptStates[i] = 0;//i-1 to hit the right state
-   }
-	return true;
+  if(i < this->noOfStates){
+    acceptStates[i] = 0;
+  }
+  return true;
 }
 	
 
 
-
+//Function to set the Alphabet
 bool GNFA::setAlphabet(char* temp){
   sigma.setAlphaSymbols(temp);
   return true;
@@ -112,50 +107,70 @@ bool GNFA::setAlphabet(alphabet my_alphabet){
 }
 
 
+alphabet GNFA::getAlphabet(){
+  return this->sigma;
+}
 
+
+
+//This function does not do much apart from print number from 0 to n-1, n being the total number of states
 void GNFA::displayStates() {
-	cout<<"# States: ";
-	for(int i=0; i< noOfStates; i++){
-		cout<<i<<", ";
-	}
-	cout<<endl;
+  cout<<"# States: ";
+  for(int i=0; i< noOfStates; i++){
+    cout<<i<<", ";
+  }
+  cout<<endl;
 }
 
+
+
+//Since the Start State is always State 0 this method only prints that starndard state
 void GNFA::displayStartState(){
-	cout<<"Start State: 0"<<endl;
+  cout<<"Start State: 0"<<endl;
 }
 
+
+
+//This function prints the list of all the accept states of the GNFA
 void GNFA::displayAcceptStates(){
-	cout<<"Accept States: ";
-	for(int i=0; i < noOfStates; i++){
-		if(acceptStates[i]){
-			cout<<i<<", "; //states are numbered from 0
-		}
-	}
-	cout<<endl;
+  cout<<"Accept States: ";
+  for(int i=0; i < noOfStates; i++){
+    if(acceptStates[i]){
+      cout<<i<<", "; //states are numbered from 0
+    }
+  }
+  cout<<endl;
 }
 
+
+
+
+//The following function displays the Alphabet
 void GNFA::displayAlphabet(){
-    cout<<"Alphabet: ";
-	cout<<sigma<<endl;
+  cout<<"Alphabet: ";
+  cout<<sigma<<endl;
 }
 
+
+
+
+//The follwoing function displays the Transition function. It gives the Start State, End State and prints the transition function. This does not display the NULL transition functions 
 void GNFA::displayTransitionFunction(){
-	cout<<"This is the transition function:"<<endl;
-	for (int i = 0 ; i < this->noOfStates; i++){
-		for (int j = 0 ; j < this->noOfStates; j++){
-			if(!transFunction[i][j]->isNULL()){
-				cout<< "	Start State :"<<i<<endl;
-				cout<< "	End State :"<<j<<endl;
-				cout<< "	Regular Expresssion :"<<transFunction[i][j]<<endl<<endl<<endl;
-			}
-			
-			
-		}
-	}
+  cout<<"This is the transition function:"<<endl;
+  for (int i = 0 ; i < this->noOfStates; i++){
+    for (int j = 0 ; j < this->noOfStates; j++){
+      if(!transFunction[i][j]->isNULL()){
+        cout<< "	Start State :"<<i<<endl;
+        cout<< "	End State :"<<j<<endl;
+        cout<< "	Regular Expresssion :"<<transFunction[i][j]<<endl<<endl<<endl;
+      }
+    }
+  }
 	
 }
 
+
+//This function is a not very neat way to displaying the Transition Function. It shows also the NULL sets
 void GNFA::forceDisplayTransitionFunction(){
 	for (int i =0; i< noOfStates; i++){
 		for (int j =0 ; j < noOfStates; j++){
@@ -165,117 +180,109 @@ void GNFA::forceDisplayTransitionFunction(){
 	
 	
 }
- 
+
+
+
+//This function is used to display the complete GNFA. ie it prints the 5 attributes that define it 
 void GNFA::display(){
-	displayStates();
-	displayStartState();	
-	displayAcceptStates();
-	displayAlphabet();
-	displayTransitionFunction();	
+  displayStates();
+  displayStartState();	
+  displayAcceptStates();
+  displayAlphabet();
+  displayTransitionFunction();	
 }
 
 
-
+//This function checks if the GNFA is nondeterministc. Using theory we found out that if any of the transitions is not a union tree, then it is not non-deterministic. That means it is also not deterministic
 bool GNFA::isNonDeterministic(){
-	for (int i = 0 ; i < this->noOfStates; i++){
-		for (int j = 0 ; i < this->noOfStates; i++){
-			if(!transFunction[i][j]->isUnionTree()){
-				return false;
-			}
-			
-		}
-	}
-	
-   return true;
+  for (int i = 0 ; i < this->noOfStates; i++){
+    for (int j = 0 ; i < this->noOfStates; i++){
+      if(!transFunction[i][j]->isUnionTree()){
+        return false;
+      }
+    }
+  }
+  return true;
 }
-	
+
+
+//This function checks to see if the GNFA is indeed deterministic. It does so by first checking if it is non-deterministic and if it is indeed non deterministic then it applies other rules. 
 bool GNFA::isDeterministic(){
-  //first find a nfa
-  //epsilon
-	if (this->isNonDeterministic()){
-	  for (int i = 0 ; i < this->noOfStates; i++){
-			multiSet temp;
-			for (int j = 0 ; i < this->noOfStates; i++){
-				temp = temp + transFunction[i][j]->getLeaves();
-			}
-			
-			if (temp[128]!=0){// this may not be required since later we do a match. 
-				return false; //currently it tests if the thing has epsilon.  
-			}
-			
-			multiSet temp1;
-			temp1 = temp1 + sigma;
-			
-			if (not(temp1 == temp)){
-				return false;
-			}
-			
-			//check if all 
-			
-		}
-		
-		
-		return true;
-	}
-	
-	else {
-		return false;
-	}
+  if (this->isNonDeterministic()){
+    for (int i = 0 ; i < this->noOfStates; i++){
+      multiSet temp;
+
+      for (int j = 0 ; i < this->noOfStates; i++){
+        temp = temp + transFunction[i][j]->getLeaves();
+      }
+      
+      if (temp[128]!=0){//128 is the place for epsilon. If there is an epslion, then it is not deterministic
+        return false; 
+      }
+      
+      multiSet temp1;
+      temp1 = temp1 + sigma;
+      
+      if (not(temp1 == temp)){
+              return false;
+      }
+      //check if all 
+    }
+    return true;
+  }
+  else {
+    return false;
+  }
 	
 }
 
-alphabet GNFA::getAlphabet(){
-  return this->sigma;
-}
 
 regexNode * GNFA::toRegexp(){
   cout<<"A"<<endl;
-	GNFA* fooGNFA = new GNFA(this->noOfStates + 2);
+  GNFA* fooGNFA = new GNFA(this->noOfStates + 2);
   cout<<"B"<<endl;
-	fooGNFA->setAcceptState(this->noOfStates + 1);
-	cout<<"C"<<endl;
+  fooGNFA->setAcceptState(this->noOfStates + 1);
+  cout<<"C"<<endl;
   fooGNFA->setAlphabet(this->getAlphabet());
-	cout<<"D"<<endl;
+  cout<<"D"<<endl;
   fooGNFA->setTransition(0, 1, new leafNode(EPSILON));
-	cout<<"E"<<endl;
+  cout<<"E"<<endl;
   
-	
-	for(int i=0; i<noOfStates; i++){
+  for(int i=0; i<noOfStates; i++){
     for(int j=0; j<noOfStates;j++){
       fooGNFA->setTransition(i+1, j+1, this->transFunction[i][j]);
-      
     }
     if(this->isAcceptState(i)){
       fooGNFA->setTransition(i, noOfStates+1, new leafNode(EPSILON));
     }
   }
-	cout<<"F"<<endl;
-  
-	cout<<"G"<<endl;
-  cout<<*fooGNFA<<endl;// to dipsly what the GNFA looks like at the end of this. 
-	cout<<"H"<<endl;
-	//from here the Floyd Warshall algorithm begins
-	
-	regexNode * result = fooGNFA->transFunction[0][noOfStates+1];
-	cout<<"I"<<endl;
-	result->simplify();
-	cout<<"J"<<endl;
-	cout<<"and result is :"<<result<<endl;
-	for(int k =1; k < noOfStates + 1; k++){
-		result = *result + *(*fooGNFA->transFunction[0][k] - *(*((*fooGNFA->transFunction[k][k])++) - *fooGNFA->transFunction[k][noOfStates +1]));
-		cout<<"k is :"<<k<<"and result is :"<<result<<endl;
-		result = result->simplify();
-		cout<<"k is :"<<k<<"and result is :"<<result<<endl;
-	}
-	cout<<"K"<<endl;
-	cout<<result<<endl;
-	delete fooGNFA;
-	// shoudl i do a delete this??
-	return result;
+
+  cout<<"F"<<endl;
+  cout<<"G"<<endl;
+  cout<<*fooGNFA<<endl; //Used to dipsly what the GNFA looks like at the end of this. 
+  cout<<"H"<<endl;
+
+  //The Floyd Warshall algorithm begins from here 
+  regexNode * result = fooGNFA->transFunction[0][noOfStates+1];
+  cout<<"I"<<endl;
+  result->simplify();
+  cout<<"J"<<endl;
+  cout<<"and result is :"<<result<<endl;
+  for(int k =1; k < noOfStates + 1; k++){
+    result = *result + *(*fooGNFA->transFunction[0][k] - *(*((*fooGNFA->transFunction[k][k])++) - *fooGNFA->transFunction[k][noOfStates +1]));
+    cout<<"k is :"<<k<<"and result is :"<<result<<endl;
+    result = result->simplify();
+    cout<<"k is :"<<k<<"and result is :"<<result<<endl;
+  }
+  cout<<"K"<<endl;
+  cout<<result<<endl;
+  delete fooGNFA;
+  // shoudl i do a delete this??
+  return result;
 	 
 	//return result here after deciding if pointer should be returned or object
 	
-/* carl sturtivant suggested the following method. argue with him the next time whether it is the best.  
+   /* carl sturtivant suggested the following method. argue with him the next time whether it is the best.  
 	for(int k = 0; k < noOfStates; k++){ 
 	cout<<"came here"<<endl;
 		for (int i = 0; i < noOfStates; i++){
@@ -305,45 +312,31 @@ regexNode * GNFA::toRegexp(){
 
 
 
-
-
-
-
-
-
-
-
 /*
 ostream& operator << (ostream& s, GNFA& a){
-	
-		s<<"# States: ";
-		for(int i=1; i<=a.noOfStates; i++){
-			s<<i<<", ";
-		}
-		s<<endl;
-	
-	s<<"Start State: 1"<<endl;
-	
-	
-	s<<"Accept States: ";
-		for(int i=0; i < a.noOfStates; i++){
-			if(a.acceptStates[i]){
-				s<<i<<", ";
-			}
-		}
-		s<<endl;
-	
-	
-	s<<a.sigma<<endl;
-	
-	return s;
+  s<<"# States: ";
+  for(int i=1; i<=a.noOfStates; i++){
+    s<<i<<", ";
+  }
+  s<<endl;
+
+  s<<"Start State: 1"<<endl;
+  s<<"Accept States: ";
+  for(int i=0; i < a.noOfStates; i++){
+    if(a.acceptStates[i]){
+      s<<i<<", ";
+    }
+  }
+  s<<endl;
+  s<<a.sigma<<endl;
+  return s;
 	
 }*/
 
 // still to figure out whether i will end up using something like the one below or the one above. 
 /*ostream& operator << (ostream& s, GNFA& a){
-	s<<a.display();
-	return s;
+  s<<a.display();
+  return s;
 }
  */
 
@@ -373,9 +366,3 @@ istream& operator >> (istream& s, GNFA& a){
  */
 
 
-
-
-
-//questions, would you want me to set a two dimensional array of regex pointers or regexes?
-//should alphabet here be the alphabet class or alphabet pointer. depending on that I will decide whether.h file goes in or .cpp files goes in?
-//is it possible for compilers to automatically prepare a .h file?
--- GNFA.h
+++ GNFA.h
@@ -10,55 +10,59 @@
 #include "multiset.h"
 
 using namespace std;
+/*
+ * This is the GNFA class. There is a regular expression on the transitions between the states
+ * The set of states is taken in the integer noOfStates. It is assumed that set of states is represented by numbers 1 to noOfStates
+ * As per the design, the Initial state is always state 0, and when there are n states, the states are named state 0 to state n-1
+ */
 
 class GNFA {
-	/*
-         * This is the GNFA class. There is a regular expression on the transitions between the states
-	 * The set of states is taken in the integer noOfStates. It is assumed that set of states is represented by numbers 1 to noOfStates
-	 * Initial state is always state 1
-	 *
-	 *
-	 */
-public:
-  int noOfStates; // the total finite set of states two dimensional arrays of regular expression
-  alphabet sigma; // How do you set a class that is inside a class? *question*//
-//bitset <20> states; // everythign that is one is a final state. how do you include a bit set that you do not know the lenght to?
-//bitset* states; // everythign that is one is a final state. how do you include a bit set that you do not know the lenght to?
-  typedef vector <regexNode*> transVector; //creates a new type of column vectors that hold the regular expression pointers called transVector; This will represent the receiveing states?. there will be one transvector for each state. it iwll store regular expresions going to other states. 
-  vector <transVector> transFunction; // this is a vector of vectors used to store the transition function. 
-  vector<bool> acceptStates;//holds in pointers 0-n-1 states 1 to n
-  //no final states
+  public:
+    int noOfStates; // The total finite set of states, as a human would count in a Finite State Machine
+    alphabet sigma; //Variable to store the alphabet 
+    //bitset <20> states; // Everything that is one is a final state. 
+    //bitset* states; // everythign that is one is a final state. how do you include a bit set that you do not know the lenght to?
+    typedef vector <regexNode*> transVector; //creates a new type of column vectors that hold the regular expression pointers called transVector; This will represent the receiveing states?. there will be one transvector for each state. it iwll store regular expresions going to other states. 
+    vector <transVector> transFunction; // this is a vector of vectors used to store the transition function. 
+    vector<bool> acceptStates;//holds in pointers 0 to n-1 states 0 to n-
 
+    //Constructor
+    GNFA(int n);
 
-  GNFA(int n);
-  bool setTransition (int i, int j, regexNode* k);
-  bool deleteTransition(int i, int j);
-	bool clearTransition (int i, int j);
-  bool setAcceptState(int i);// remember states are numbered 0 to n-1
-  bool isAcceptState(int i);
-  bool resetAcceptState(int i);
-  bool setAlphabet(char* temp);
-  bool setAlphabet(alphabet);
-  alphabet getAlphabet();
-  void displayStates();
-  void displayStartState();	
-  void displayAcceptStates();
-  void displayAlphabet();
-  void displayTransitionFunction();
-	void forceDisplayTransitionFunction();
-  void display();	
-  bool isDeterministic();
-  bool isNonDeterministic();
-  regexNode * toRegexp();
-	//friend void operator << (ostream&, GNFA& );
-	friend ostream& operator << (ostream&, GNFA& );
-	friend istream& operator >> (istream& , GNFA& );
+    //Transition Functions
+    bool setTransition (int i, int j, regexNode* k);
+    bool deleteTransition(int i, int j);
+    bool clearTransition (int i, int j);
+
+    //Functions for States
+    bool setAcceptState(int i);// remember states are numbered 0 to n-1
+    bool isAcceptState(int i);
+    bool resetAcceptState(int i);
+    
+    //Functions for the Alphabet
+    bool setAlphabet(char* temp);
+    bool setAlphabet(alphabet);
+    alphabet getAlphabet();
+
+    //The following are the functions ued for display purposes
+    void displayStates();
+    void displayStartState();	
+    void displayAcceptStates();
+    void displayAlphabet();
+    void displayTransitionFunction();
+    void forceDisplayTransitionFunction();
+    void display();	
+
+    //Operations
+    bool isNonDeterministic();
+    bool isDeterministic();
+    regexNode * toRegexp();
+
+    //Friend void operator << (ostream&, GNFA& );
+    friend ostream& operator << (ostream&, GNFA& );
+    friend istream& operator >> (istream& , GNFA& );
 };
 
 //void operator << (ostream&, GNFA& );
 ostream& operator << (ostream&, GNFA& );
-
-istream& operator >> (istream& , GNFA& );
-//questions, would you want me to set a two dimensional array of regex pointers or regexes?
-//should alphabet here be the alphabet class or alphabet pointer. depending on that I will decide whether.h file goes in or .cpp files goes in?is//is it possible for compilers to automatically prepare a .h file?
-
+istream& operator >> (istream&, GNFA& );
