3bae809993adea378e22a7ac868e837f06bdb3ba
Gagan Awhad <regist.gagan@gmail.com>
2010-10-04 18:00:04 -0500

    Added setTransition function

--- GNFA.cpp
+++ GNFA.cpp
@@ -29,12 +29,20 @@ GNFA::GNFA(int n){
     transFunction.push_back(temp);
   }
   
-  cout<<"this is 0 4 on creation";
-  transFunction[0][4]->display();
-  cout<<endl;
 
 }
-	
+
+bool GNFA::setTransitionPtr(int i, int j, regexNode* k){
+  if (i < this->noOfStates && j < this->noOfStates){
+    transFunction[i][j] = k;	
+    return true;
+  }
+  else {
+    cout<<"Either one or both of the states are not in the rage"<<endl;
+    return false;
+  }
+
+}
 
 
 //Take two states and regular expression and then set the transiontion function
