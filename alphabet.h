4628edacec32d0075880cd26f53b2f7658793925
Gagan Awhad <regist.gagan@gmail.com>
2010-10-20 11:32:47 -0500

    Changed Indentation

--- alphabet.h
+++ alphabet.h
@@ -15,32 +15,32 @@
 using namespace std;
 
 class alphabet{
-	private:  
-          bitset<129> alphaSymbols; //there is some sort of an assumption that there will be 128 characters only 
-		//the last bitset is reserved for epsilons
-		// this display for the bitset is as you would read a binay number. setting 128 will end up setting the msb and will be seen first in the array. 
-	public:
-	alphabet();
-	alphabet (char * );		
-	bool setAlphaSymbols (char * );
-	bool setAlphaSymbol (int );
-	alphabet operator + (alphabet );
+  private:  
+    bitset<129> alphaSymbols; //there is some sort of an assumption that there will be 128 characters only 
+          //the last bitset is reserved for epsilons
+          // this display for the bitset is as you would read a binay number. setting 128 will end up setting the msb and will be seen first in the array. 
+  public:
+  alphabet();
+  alphabet (char * );		
+  bool setAlphaSymbols (char * );
+  bool setAlphaSymbol (int );
+  alphabet operator + (alphabet );
 
 
-	alphabet operator * (alphabet );
+  alphabet operator * (alphabet );
 
 
-	alphabet operator - (alphabet );
-	
-	bool isMember(int );
-	
-	bool isMember(char );
-	
-	
-	void disp ();
-	
-	friend ostream& operator << (ostream& , alphabet& );
-	friend istream& operator >> (istream& , alphabet& );
+  alphabet operator - (alphabet );
+  
+  bool isMember(int );
+  
+  bool isMember(char );
+  
+  
+  void disp ();
+  
+  friend ostream& operator << (ostream& , alphabet& );
+  friend istream& operator >> (istream& , alphabet& );
 
 
 };
